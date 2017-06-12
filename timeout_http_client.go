e18dafe44b7bd77976269dfdfc89be5a72ed38bb
Jiujiang Yuan <jiujiangyuan@qq.com>
2017-06-12 16:42:25 +0800

    add timeout_http_client

---
+++ timeout_http_client.go
@@ -0,0 +1,40 @@
+package tools
+
+import (
+    "net"
+    "net/http"
+    "time"
+)
+
+func NewTimeOutHttpClient(dailTimeout, readTimeout, writeTimeout time.Duration) *http.Client {
+    // TODO: Check valid
+    transport := &http.Transport{
+        Dial: func(network, addr string) (net.Conn, error) {
+            c, err := net.DialTimout(network, addr, dailTimeout)
+            if err != nil {
+                return nil, err
+            }
+
+            return &timeoutConn{readTimeout, writeTimeout, c}, nil
+        },
+    }
+
+    return &http.Client{Transport: transport}
+}
+
+type timeoutConn struct {
+    readTimeout      time.Duration
+    writeTimeout     time.Duration
+
+    net.Conn
+}
+
+func (c *timeoutConn) Read(b []byte) (n int, err error) {
+    c.Conn.SetReadDeadline(time.Now().Add(c.readTimeout))
+    return c.Conn.Read(b)
+}
+
+func (c *timeoutConn) Write(b []byte) (n int, err error) {
+    c.Conn.SetWriteDeadline(time.Now().Add(c.writeTimeout))
+    return c.Conn.Write(b)
+}
